0844473061cf78f75744b5f58e33aec190161798
Aleks-Kar <Nomas86@yandex.ru>
2022-09-22 12:09:01 +0300

    fix: data folder path issue

--- src/components/user-card.tmp.js
+++
@@ -1,8 +0,0 @@
-import hbs from 'handlebars';
-// import './card.css';
-
-const template = `
-<div class="user-card">я шаблон user-card!</div>
-`;
-
-hbs.registerPartial('userCard', template);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-// import hbs from '@anikin/parcel-transformer-handlebars';
-import hbs from 'handlebars';
+import hbs from '@anikin/parcel-transformer-handlebars';
 import page404 from './pages/404.tmp';
 
 const data = {
   name: 'Aleks'
 };
 
-const template = hbs.compile(page404);
-const html = template(data);
+const renderFunction = hbs.compile(page404);
+const html = renderFunction(data);
 
 const rootNode = document.querySelector('#root');
 rootNode.innerHTML = html;
