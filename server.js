94dab0f9049ba9b53be98b7219b48de0b40c8bdc
Aleks-Kar <Nomas86@yandex.ru>
2022-09-29 06:27:49 +0300

    refactor: server.js

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require("express");
-const path = require("path");
 const port = 3000;
-
 const app = express();
 
-app.use(express.static(path.join(__dirname, "static")));
+app.use(express.static(__dirname + "/dist"));
 
 app.listen(port, () => {
   console.log(`Приложение слушает порт: ${port}`);
